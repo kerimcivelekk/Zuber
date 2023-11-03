dcc129dfd3509dfe3d6d61a26481172d2539aaed
Kerim <targetbad@hotmail.com>
2023-11-03 18:17:57 +0300

    Initial Commit

---
+++ UberClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by Kerim Civelek on 3.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
